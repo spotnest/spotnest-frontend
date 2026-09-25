a75ee73a08e9b1b8db7af523f82ea4a110497c6e
Fibin <fibinkunnath@gmail.com>
2026-09-25 10:35:48 +0530

    Fix owner registration document upload

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -11,9 +11,6 @@ const API_BASE_URL =
 const api: AxiosInstance = axios.create({
     baseURL: API_BASE_URL,
     withCredentials: true,
-    headers: {
-        "Content-Type": "application/json",
-    },
 });
 
 /**
